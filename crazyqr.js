6779472426f467f48c3a8313fa2fc88e74e36ed2
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-07-04 02:07:01 +0100

    Update crazyqr.js

--- crazyqr.js
+++ crazyqr.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
+				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: 'CRAZY-MD-WHATSAPP-BOT;;;=>' + b64data });
 	
 				   let WASI_MD_TEXT = `
 *_Session Connected By CRAZY MD_*
